9958cf4868d91b82ba23d7ba7d072ccd6ebda5ff
Duong Minh Quan <91663460+2uanDM@users.noreply.github.com>
2023-12-16 15:12:56 +0700

    Fix all missing product

---
+++ src/crawler/base.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+import os
+import json
+import sys
+import sqlite3
+sys.path.append(os.getcwd())  # NOQA
+
+
+class BaseCrawler(ABC):
+    """
+        Base class for all crawlers.
+    """
+
+    CONFIG_DIR = os.path.join(os.getcwd(), 'config')
+    DATABASE_DIR = os.path.join(os.getcwd(), 'database')
+
+    def load_config(self, filename) -> dict:
+        """
+            Load the config file from config directory.
+        """
+        with open(os.path.join(self.CONFIG_DIR, filename), 'r') as f:
+            return json.load(f)
+
+    def connect_db(self, filename) -> sqlite3.Connection:
+        """
+            Connect to the database.
+        """
+        db_path = os.path.join(self.DATABASE_DIR, filename)
+        return sqlite3.connect(db_path)
+
+    def log(self, msg):
+        datefmt = datetime.strftime(datetime.now(), format='%Y-%m-%d %H:%M:%S')
+        # Print to console
+        print(f'[{datefmt}]: {msg}')
